438ff798cebe6b31fd51ade9c9027d5d199a43cd
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-01-10 21:52:01 +0200

    List terraform service account as dataset owner

--- terraform/modules/stock_market_research/service_account.tf
+++ terraform/modules/stock_market_research/service_account.tf
@@ -3,3 +3,9 @@ resource "google_service_account" "stock_market_research_user" {
   account_id = "stock-market-research-user"
   display_name = "Stock market research user"
 }
+
+
+resource "google_service_account" "new_user" {
+  account_id = "new-user"
+  display_name = "Stock market research user"
+}
