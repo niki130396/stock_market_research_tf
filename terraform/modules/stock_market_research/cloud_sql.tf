07ceb6a484924a9ab238a4455707be3411887d4a
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-01-20 11:19:45 +0200

    Create cloud sql instance

---
+++ terraform/modules/stock_market_research/cloud_sql.tf
@@ -0,0 +1,17 @@
+
+
+resource "google_sql_database_instance" "stock_market_research_db" {
+  name                = "stock-market-research-db"
+  database_version    = "POSTGRES_15"
+  region              = "europe-west1"
+  deletion_protection = false
+
+  settings {
+    tier = "db-f1-micro"
+
+  ip_configuration {
+      ipv4_enabled = true
+      require_ssl  = false  # Disable SSL for public IP (Note: This is not recommended for production environments)
+    }
+  }
+}
