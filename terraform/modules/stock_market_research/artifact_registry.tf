a1ae0e1d67a9fb68533b06119c0ea8497d5a2d1f
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-03-03 17:42:29 +0200

    Activate API

--- terraform/modules/stock_market_research/artifact_registry.tf
+++ terraform/modules/stock_market_research/artifact_registry.tf
@@ -2,4 +2,6 @@ resource "google_artifact_registry_repository" "docker" {
   location      = "europe-north1"
   repository_id = "docker"
   format        = "DOCKER"
+
+  depends_on = [google_project_service.enabled_apis]
 }
--- terraform/modules/stock_market_research/iam.tf
+++ terraform/modules/stock_market_research/iam.tf
@@ -7,4 +7,6 @@ resource "google_artifact_registry_repository_iam_member" "docker_writer" {
   repository = google_artifact_registry_repository.docker.name
   role = "roles/artifactregistry.writer"
   member = each.value
+
+  depends_on = [google_artifact_registry_repository.docker]
 }
