a832ffc72661219e372b3ab9d265d39fb36ae8b5
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-03-04 10:35:55 +0200

    Create cloud run job

--- terraform/modules/stock_market_research/iam.tf
+++ terraform/modules/stock_market_research/iam.tf
@@ -1,7 +1,6 @@
 resource "google_artifact_registry_repository_iam_member" "docker_writer" {
   for_each = toset([
     "serviceAccount:${google_service_account.stock_market_research_user.email}",
-    "serviceAccount:service-824941781846@serverless-robot-prod.iam.gserviceaccount.com",
   ])
   project = google_artifact_registry_repository.docker.project
   location = google_artifact_registry_repository.docker.location
