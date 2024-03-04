10c00c088cb2a3040cc60554ffcd2cf7b643cf26
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-03-04 10:29:35 +0200

    Create cloud run job

---
+++ terraform/modules/stock_market_research/cloud_run.tf
@@ -0,0 +1,22 @@
+
+locals {
+  stock_market_research_labels = {
+    "group" : "stock_market_research"
+  }
+}
+
+resource "google_cloud_run_v2_job" "get_financial_statements" {
+  name = "get-financial-statements"
+  location = "europe-north1"
+
+  labels = local.stock_market_research_labels
+
+  template {
+    template {
+      containers {
+        image = "europe-north1-docker.pkg.dev/stock-market-research-410417/docker/stock_market_research:latest"
+      }
+      service_account = google_service_account.stock_market_research_user.email
+    }
+  }
+}
