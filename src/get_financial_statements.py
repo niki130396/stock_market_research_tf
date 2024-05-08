8c36bee2e873321ddd47f1c6975eda95268bb645
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-05-08 09:25:08 +0300

    Test request

--- src/get_financial_statements.py
+++ src/get_financial_statements.py
@@ -9,36 +9,57 @@ from src.utils.sql_helpers import (
     connect_to_cloud_sql,
     get_companies_to_retrieve,
 )
-from src.utils.requests import get_company_information
+from src.utils.requests import financial_modeling_prep_async_request
 from src.models.fundamentals import (
-    CompanyMetaData,
+    StatementTypeDefinition,
+    RetrievedStatementsLog,
 )
 
 
 if __name__ == "__main__":
     engine = connect_to_cloud_sql()
 
+    with Session(engine) as session:
+        income_statement = session.query(StatementTypeDefinition).filter_by(statement_type="income_statement").first()
+        balance_sheet = session.query(StatementTypeDefinition).filter_by(statement_type="balance_sheet_statement").first()
+        cash_flow_statement = session.query(StatementTypeDefinition).filter_by(statement_type="cash_flow_statement").first()
+
     # Since we're running it every day we are going to retrieve one statement type for each day.
     STATEMENTS_MAPPING = {
-        1: "income_statement",
-        2: "balance_sheet_statement",
-        3: "cash_flow_statement"
+        1: income_statement,
+        2: balance_sheet,
+        3: cash_flow_statement
     }
 
-    STATEMENT_TYPE_TO_RETRIEVE = STATEMENTS_MAPPING[(date.today().day % 3) - 1]
+    statement_type_object = STATEMENTS_MAPPING[(date.today().day % 3) - 1]
 
     companies_to_retrieve = get_companies_to_retrieve(
         engine=engine,
-        statement_type=STATEMENT_TYPE_TO_RETRIEVE
+        statement_type=statement_type_object.statement_type
     )
 
-    endpoint = LOCAL_STATEMENT_TYPE_TO_ENDPOINT_MAPPING[STATEMENT_TYPE_TO_RETRIEVE]
-    results = get_company_information(
+    endpoint = LOCAL_STATEMENT_TYPE_TO_ENDPOINT_MAPPING[statement_type_object.statement_type]
+    results, _ = asyncio.run(financial_modeling_prep_async_request(
         endpoint=endpoint,
         symbols=companies_to_retrieve,
         apikey=environ["FINANCIAL_MODELING_PREP_API_TOKEN"],
         period="annual"
-    )
+    ))
+
+    retrieved_statement_logs = []
+    for symbol in companies_to_retrieve:
+        obj = RetrievedStatementsLog(
+            symbol=symbol,
+            statement_type=statement_type_object.id
+        )
+        retrieved_statement_logs.append(obj)
+
+    with Session(engine) as session:
+        session.add_all(retrieved_statement_logs)
+        session.commit()
+
+
+
     # start from the companies that we haven't attempted. We will only try companies from the US market,
     # because those are the ones we will have in the CompanyMetaData table anyway
     # The table should have columns that signal whether we have each of the statements available for a given company
---
+++ src/test_request.py
@@ -0,0 +1,9 @@
+import os
+import requests
+from urllib.parse import urlencode
+
+api_token = os.environ["FINANCIAL_MODELING_PREP_API_TOKEN"]
+
+response = requests.get(f"https://financialmodelingprep.com/api/v3/profile/ACCMF?{urlencode({'apikey': api_token})}")
+print(response.json())
+print(response.text)
