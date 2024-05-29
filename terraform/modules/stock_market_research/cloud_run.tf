3a045c74919fdfa874c644f867725d4da2fafe7e
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-05-29 16:10:33 +0300

    Create google_cloud_run_v2_service to host plotly dash application

--- terraform/modules/stock_market_research/cloud_run.tf
+++ terraform/modules/stock_market_research/cloud_run.tf
@@ -54,36 +54,18 @@ resource "google_cloud_run_v2_job" "get_company_details" {
 }
 
 
-resource "google_cloud_run_v2_job" "test_request" {
-  name = "test-request"
+resource "google_cloud_run_v2_service" "stock_market_research_dashboard" {
+  name = "stock-market-research-dashboard"
   location = var.gcp_region
+  ingress = "INGRESS_TRAFFIC_ALL"
 
   labels = local.stock_market_research_labels
 
   template {
     template {
       containers {
-        image   = "europe-north1-docker.pkg.dev/stock-market-research-410417/docker/stock_market_research:latest"
-        command = ["python", "src/test_request.py"]
-
-        env {
-          name  = "STOCK_MARKET_RESEARCH_DB_USER_PASSWORD"
-          value = var.stock_market_research_db_user_password
-        }
-        env {
-          name = "STOCK_MARKET_RESEARCH_DB_USERNAME"
-          value = google_sql_user.stock_market_research_db_user.name
-        }
-        env {
-          name = "STOCK_MARKET_RESEARCH_DB_HOST"
-          value = google_sql_database_instance.stock_market_research_db.connection_name
-        }
-        env {
-          name = "FINANCIAL_MODELING_PREP_API_TOKEN"
-          value = var.financial_modeling_prep_api_token
-        }
+        image = "europe-north1-docker.pkg.dev/stock-market-research-410417/docker/stock_market_research_dashboard:latest"
       }
-
       service_account = google_service_account.stock_market_research_user.email
     }
   }
