11f4862ebb92ecb190aa52a1994d072a112d1d44
nikolay-petrov96 <nikolay.petrov@ovo.com>
2024-05-31 10:06:32 +0300

    Set up cleaup policy for artifact registry

--- terraform/modules/stock_market_research/artifact_registry.tf
+++ terraform/modules/stock_market_research/artifact_registry.tf
@@ -1,5 +1,22 @@
+#resource "google_artifact_registry_repository" "docker" {
+#  location      = "europe-north1"
+#  repository_id = "docker"
+#  format        = "DOCKER"
+#
+#  cleanup_policy_dry_run = false
+#  cleanup_policies {
+#    id     = "keep-minimum-versions"
+#    action = "KEEP"
+#    most_recent_versions {
+#      keep_count = 5
+#    }
+#  }
+#  depends_on = [google_project_service.enabled_apis]
+#}
+
+
 resource "google_artifact_registry_repository" "docker" {
-  location      = "europe-north1"
+  location      = var.gcp_region
   repository_id = "docker"
   format        = "DOCKER"
 
