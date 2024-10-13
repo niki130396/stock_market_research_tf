7f472f3b97c62a2909342ee2dfaab7a234d53d03
nikolay <niki130396@gmail.com>
2024-10-13 12:22:14 +0300

    delete resources

--- terraform/modules/stock_market_research/service_account.tf
+++ terraform/modules/stock_market_research/service_account.tf
@@ -3,8 +3,3 @@ resource "google_service_account" "stock_market_research_user" {
   account_id = "stock-market-research-user"
   display_name = "Stock market research user"
 }
-
-
-resource "google_service_account" "secondary_terraform" {
-  account_id = "secondary-terraform"
-}
